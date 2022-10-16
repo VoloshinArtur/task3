28deff483f87b420709d92f429b82c2eca3047d8
VoloshinArtur <115408461+VoloshinArtur@users.noreply.github.com>
2022-10-16 17:26:59 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main(){	
+	int W, H, R,D;
+	cin >> W >> H >> R;
+	D = R * 2;
+	if (W >= D && H >= D)cout << "YES";
+	else
+	{
+		cout << "NO";
+	}
+}
+
